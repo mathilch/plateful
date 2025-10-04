058eff0e97621a60cb0bd941b364e9bb376a5153
Rameez Ahmed <rameezahmed349@hotmail.com>
2025-10-04 13:17:13 +0200

    added folder for entities

---
+++ src/UsersMicroservice/Users.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Users.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public bool IsActive { get; set; }
+}
