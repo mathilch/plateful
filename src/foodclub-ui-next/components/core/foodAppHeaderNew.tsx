dd3f0bab713f0424ab6bd215b5f5f02b18f6b558
daniil <danilkalinin@gmail.com>
2025-10-15 13:20:35 +0200

    add default colors to the theme

--- src/foodclub-ui-next/components/core/foodAppHeaderNew.tsx
+++ src/foodclub-ui-next/components/core/foodAppHeaderNew.tsx
@@ -18,7 +18,7 @@ export default function FoodAppHeaderNew() {
             <header className="flex justify-evenly items-center gap-5 h-20 outline-1 outline-gray-200">
 
                 <div className="flex h-20 gap-4 items-center">
-                    <Link href="/" className="text-gray-800 text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">
+                    <Link href="/" className="text-primary-gray text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">
                         <Image
                             src="/source_logo_plateful.png"
                             alt="Logo"
@@ -26,18 +26,18 @@ export default function FoodAppHeaderNew() {
                             height={73}
                         />
                     </Link>
-                    <Link href="/" className="py-4 px-1 text-emerald-800 text-xl font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Plateful DK</Link>
+                    <Link href="/" className="py-4 px-1 text-primary-green text-xl font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Plateful DK</Link>
                 </div>
 
                 <div className="flex h-14 gap-10">
-                    <Link href="#" className="py-4 px-1 text-gray-800 text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Discover</Link>
-                    <Link href="/createFoodEvent" className="py-4 px-1 text-gray-800 text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Host a Meal</Link>
-                    <Link href="#" className="py-4 px-1 text-gray-800 text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">How it Works</Link>
+                    <Link href="#" className="py-4 px-1 text-primary-gray text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Discover</Link>
+                    <Link href="/createFoodEvent" className="py-4 px-1 text-primary-gray text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Host a Meal</Link>
+                    <Link href="#" className="py-4 px-1 text-primary-gray text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">How it Works</Link>
                 </div>
 
                 <div className="flex gap-4">
-                    <Link href="#" onClick={() => setOpenAuthDialog(true)} className="py-2 px-8 border-1 border-emerald-800 rounded-[50%] text-emerald-800 text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Log in</Link>
-                    <Link href="#" className="py-2 px-12 border-1 border-black text-white text-base font-bold font-['Poppins'] bg-emerald-800 rounded-xl hover:bg-muted hover:text-foreground transition-colors">Sign up</Link>
+                    <Link href="#" onClick={() => setOpenAuthDialog(true)} className="py-2 px-8 border-1 border-primary-green rounded-[50%] text-primary-green text-base font-bold font-['Poppins'] hover:bg-muted hover:text-foreground transition-colors">Log in</Link>
+                    <Link href="#" className="py-2 px-12 border-1 border-black text-white text-base font-bold font-['Poppins'] bg-primary-green rounded-xl hover:bg-muted hover:text-foreground transition-colors">Sign up</Link>
                 </div>
 
 
