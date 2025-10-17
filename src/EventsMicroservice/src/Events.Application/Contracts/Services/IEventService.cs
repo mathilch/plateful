11f878de0fb2538a6c29d2cbe6232b7063537445
mathi <mathiaslucht6@gmail.com>
2025-10-17 15:44:37 +0200

    Better var naming and better checks

--- src/EventsMicroservice/src/Events.Application/Contracts/Services/IEventService.cs
+++ src/EventsMicroservice/src/Events.Application/Contracts/Services/IEventService.cs
@@ -7,16 +7,16 @@ namespace Events.Application.Contracts.Services;
 public interface IEventService
 {
     // Basic CRUD
-    Task<EventDto> AddEvent(bool isUserLoggedIn, CreateEventRequestDto createEvent);
-    Task<EventDto> GetEventByEventId(bool isUserLoggedIn, Guid eventId);
-    Task<List<EventDto>> GetEventsByUserId(bool isUserLoggedIn, Guid userId);
+    Task<EventDto> AddEvent(Guid loggedInUserId, CreateEventRequestDto createEvent);
+    Task<EventDto> GetEventByEventId(Guid loggedInUserId, Guid eventId);
+    Task<List<EventDto>> GetEventsByUserId(Guid loggedInUserId, Guid userId);
     Task<List<EventDto>> GetAllEvents();
-    Task<EventDto> UpdateEvent(Guid userId, Guid eventId, UpdateEventRequestDto updateReq);
-    Task<EventDto> DeleteEvent(Guid userId, Guid eventId);
+    Task<EventDto> UpdateEvent(Guid loggedInUserId, Guid eventId, UpdateEventRequestDto updateReq);
+    Task<EventDto> DeleteEvent(Guid loggedInUserId, Guid eventId);
     
     // Event specific
-    Task<EventDto> MakeEventPrivate(Guid userId, Guid eventId);
-    Task<EventDto> MakeEventPublic(Guid userId, Guid eventId);
-    Task<EventDto> CancelEvent(Guid userId, Guid eventId);
+    Task<EventDto> MakeEventPrivate(Guid loggedInUserId, Guid eventId);
+    Task<EventDto> MakeEventPublic(Guid loggedInUserId, Guid eventId);
+    Task<EventDto> CancelEvent(Guid loggedInUserId, Guid eventId);
 
 }
--- src/EventsMicroservice/src/Events.Application/Services/EventService.cs
+++ src/EventsMicroservice/src/Events.Application/Services/EventService.cs
@@ -2,42 +2,57 @@ using Events.Application.Contracts.Repositories;
 using Events.Application.Contracts.Services;
 using Events.Application.Dtos;
 using Events.Application.Dtos.Requests;
-using Events.Domain.Entities;
 using Events.Application.Exceptions;
 
 namespace Events.Application.Services;
 
 public class EventService(IEventRepository eventRepository) : IEventService
 {
-    public Task<EventDto> AddEvent(bool isUserLoggedIn, CreateEventRequestDto createEvent) =>
-        isUserLoggedIn
-            ? eventRepository.AddEvent(createEvent)
-            : throw new UnauthorizedAccessException($"You need to be logged in to create an event");
+    public Task<EventDto> AddEvent(Guid loggedInUserId, CreateEventRequestDto createEvent)
+    {
+        if (loggedInUserId == Guid.Empty)
+        {
+            throw new UnauthorizedAccessException($"You need to be logged in to create an event");
+        }
+        return eventRepository.AddEvent(createEvent with { UserId = loggedInUserId });
+    }
 
-    public Task<EventDto> GetEventByEventId(bool isUserLoggedIn, Guid eventId) =>
-        isUserLoggedIn 
-            ? eventRepository.GetEventById(eventId)
-            : throw new UnauthorizedAccessException($"You need to be logged in to view an event");
-    
+    public Task<EventDto> GetEventByEventId(Guid loggedInUserId, Guid eventId)
+    {
+        if (loggedInUserId == Guid.Empty)
+        {
+            throw new UnauthorizedAccessException($"You need to be logged in to view an event");
+        }
 
-    public Task<List<EventDto>> GetEventsByUserId(bool isUserLoggedIn, Guid userId) =>
-        isUserLoggedIn
-            ? eventRepository.GetEventsByUserId(userId)
-            : throw new UnauthorizedAccessException($"You need to be logged in to view all the events for user with id: {userId}");
-    
+        return eventRepository.GetEventById(eventId);
+    }
+
+    // TODO Also retreive events where the loggedInUser exists in the participation list.
+    // Need to factor Event Entity to contain a list of participants
+    public async Task<List<EventDto>> GetEventsByUserId(Guid loggedInUser, Guid loggedInUserId)
+    {
+        if (loggedInUserId == Guid.Empty)
+        {
+            throw new UnauthorizedAccessException($"You need to be logged in to view all the events for user with id: {loggedInUserId}");
+        }
+        var events = await eventRepository.GetEventsByUserId(loggedInUserId);
+        return events
+            .Where(e => e.IsPublic)
+            .ToList();
+    }
 
     public Task<List<EventDto>> GetAllEvents()
     {
         return eventRepository.GetAllEvents();
     }
 
-    // UserId getting carried into from the auth service
-    public async Task<EventDto> UpdateEvent(Guid currentUserId, Guid eventId, UpdateEventRequestDto updateReq)
+    // loggedInUserId getting carried into from the auth service
+    public async Task<EventDto> UpdateEvent(Guid currentloggedInUserId, Guid eventId, UpdateEventRequestDto updateReq)
     {
         var e =  await eventRepository.GetEventById(eventId);
-        if(e.UserId != currentUserId)
+        if(e.UserId != currentloggedInUserId)
         {
-            throw new UnauthorizedUserException(e.EventId, e.UserId, currentUserId);
+            throw new UnauthorizedUserException(e.EventId, e.UserId, currentloggedInUserId);
         } 
         return await eventRepository.UpdateEvent(eventId, @event =>
         {
@@ -53,22 +68,22 @@ public class EventService(IEventRepository eventRepository) : IEventService
         });
     }
 
-    public async Task<EventDto> DeleteEvent(Guid currentUserId, Guid eventId)
+    public async Task<EventDto> DeleteEvent(Guid currentloggedInUserId, Guid eventId)
     {
         var e =  await eventRepository.GetEventById(eventId);
-        if(e.UserId != currentUserId)
+        if(e.UserId != currentloggedInUserId)
         {
-            throw new UnauthorizedUserException(e.EventId, e.UserId, currentUserId);
+            throw new UnauthorizedUserException(e.EventId, e.UserId, currentloggedInUserId);
         } 
         return await eventRepository.DeleteEvent(eventId);
     }
 
-    public async Task<EventDto> MakeEventPrivate(Guid currentUserId, Guid eventId)
+    public async Task<EventDto> MakeEventPrivate(Guid currentloggedInUserId, Guid eventId)
     {
         var e =  await eventRepository.GetEventById(eventId);
-        if(e.UserId != currentUserId)
+        if(e.UserId != currentloggedInUserId)
         {
-            throw new UnauthorizedUserException(e.EventId, e.UserId, currentUserId);
+            throw new UnauthorizedUserException(e.EventId, e.UserId, currentloggedInUserId);
         }
 
         if (!e.IsPublic)
@@ -79,12 +94,12 @@ public class EventService(IEventRepository eventRepository) : IEventService
         return await eventRepository.UpdateEvent(eventId, @event => @event.IsPublic = false);
     }
 
-    public async Task<EventDto> MakeEventPublic(Guid currentUserId, Guid eventId)
+    public async Task<EventDto> MakeEventPublic(Guid currentloggedInUserId, Guid eventId)
     {
         var e =  await eventRepository.GetEventById(eventId);
-        if(e.UserId != currentUserId)
+        if(e.UserId != currentloggedInUserId)
         {
-            throw new UnauthorizedUserException(e.EventId, e.UserId, currentUserId);
+            throw new UnauthorizedUserException(e.EventId, e.UserId, currentloggedInUserId);
         }
 
         if (e.IsPublic)
@@ -96,12 +111,12 @@ public class EventService(IEventRepository eventRepository) : IEventService
     }
 
     // TODO Check if participants have already paid
-    public async Task<EventDto> CancelEvent(Guid currentUserId, Guid eventId)
+    public async Task<EventDto> CancelEvent(Guid currentloggedInUserId, Guid eventId)
     {
         var e = await eventRepository.GetEventById(eventId);
-        if (e.UserId != currentUserId)
+        if (e.UserId != currentloggedInUserId)
         {
-            throw new UnauthorizedUserException(e.EventId, e.UserId, currentUserId);
+            throw new UnauthorizedUserException(e.EventId, e.UserId, currentloggedInUserId);
         }
         
         return await eventRepository.UpdateEvent(eventId, @event => @event.IsActive = false);
