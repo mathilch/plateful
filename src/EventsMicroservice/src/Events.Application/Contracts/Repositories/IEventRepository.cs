bfc4db934737ac2e258fe40a0c4f9d6029b9ad82
Rameez Ahmed <rameezahmed349@hotmail.com>
2025-10-26 17:23:58 +0100

    fix issues with the event repository so that clients can get or create events in database

--- src/EventsMicroservice/src/Events.Application/Contracts/Repositories/IEventRepository.cs
+++ src/EventsMicroservice/src/Events.Application/Contracts/Repositories/IEventRepository.cs
@@ -1,5 +1,4 @@
 using Events.Application.Dtos;
-using Events.Application.Dtos.Requests;
 using Events.Domain.Entities;
 
 namespace Events.Application.Contracts.Repositories;
@@ -8,7 +7,7 @@ public interface IEventRepository
 {
     Task<EventDto> GetEventById(Guid id);
     Task<List<EventDto>> GetAllEvents();
-    Task<EventDto> AddEvent(CreateEventRequestDto createEvent);
+    Task<EventDto> AddEvent(Event createEvent);
     Task<EventDto> UpdateEvent(Guid id, Action<Event> op);
     Task<EventDto> DeleteEvent(Guid id);
     Task<List<EventDto>> GetEventsByUserId(Guid userId);
