659a22320fd1a72c485dc20284a5903976fe7328
daniil <danilkalinin@gmail.com>
2025-10-29 19:15:32 +0100

    add address autocomplete

--- src/foodclub-ui-next/components/core/createFoodEventForms/whenWhereForm.tsx
+++ src/foodclub-ui-next/components/core/createFoodEventForms/whenWhereForm.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import ComponentsWrapper from "../wrappers/componentsWrapper";
 import OrangeWrapper from "../wrappers/orangeWrapper";
 import CreateEventFormInput from "./createEventFormInput";
+import InputAddressAutocomplete from "./inputAddressAutocomplete";
 
+export interface LocationDetails {
+    streetNumber?: string;
+    postalCode?: string;
+    city?: string;
+    region?: string;
+}
 
 export default function WhenWhereForm() {
+    const [formLocationDetails, setFormLocationDetails] = useState<LocationDetails | null>(null);
+
     return (
         // md:grid-cols-[2fr_1fr]
         <div id="mainWrapper" className="grid grid-cols-1 md:grid-cols-[1fr_auto] gap-10">
@@ -39,11 +51,21 @@ export default function WhenWhereForm() {
 
                 {/* TODO: work on address selection options, eg: show list of regions */}
 
+                <InputAddressAutocomplete
+                    labelText={"Address autocomplete test"}
+                    id={"addressAutocomplete"}
+                    setFormLocationDetailsAction={setFormLocationDetails} />
+
                 <CreateEventFormInput
                     id="address"
                     labelText="Address"
                     type="text"
                     placeholder="Street & number"
+                    value={formLocationDetails?.streetNumber}
+                    onChange={e => setFormLocationDetails(prev => ({
+                        ...prev,
+                        streetNumber: e.target.value
+                    }))}
                     required
                 />
 
@@ -54,6 +76,11 @@ export default function WhenWhereForm() {
                         labelText="Postal code"
                         type="text"
                         placeholder="2200"
+                        value={formLocationDetails?.postalCode}
+                        onChange={e => setFormLocationDetails(prev => ({
+                            ...prev,
+                            postalCode: e.target.value
+                        }))}
                         required
                     />
 
@@ -62,6 +89,11 @@ export default function WhenWhereForm() {
                         labelText="City"
                         type="text"
                         placeholder="Copenhagen"
+                        value={formLocationDetails?.city}
+                        onChange={e => setFormLocationDetails(prev => ({
+                            ...prev,
+                            city: e.target.value
+                        }))}
                         required
                     />
 
@@ -70,6 +102,11 @@ export default function WhenWhereForm() {
                         labelText="Region"
                         type="text"
                         placeholder="Capital Region"
+                        value={formLocationDetails?.region}
+                        onChange={e => setFormLocationDetails(prev => ({
+                            ...prev,
+                            region: e.target.value
+                        }))}
                         required
                     />
 
@@ -78,6 +115,8 @@ export default function WhenWhereForm() {
                         labelText="Country"
                         type="text"
                         placeholder="Denmark"
+                        value="Denmark"
+                        disabled
                         required
                     />
 
