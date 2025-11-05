16cae51f05e6241c706a6238fc922be6d1fa41ba
daniil <danilkalinin@gmail.com>
2025-11-05 16:06:53 +0100

    fix buttons

--- src/foodclub-ui-next/app/createFoodEvent/layout.tsx
+++ src/foodclub-ui-next/app/createFoodEvent/layout.tsx
@@ -11,18 +11,16 @@ export default function CreateFormLayout({
   children: React.ReactNode;
 }>) {
 
-  const activeButtonState = useState<string>("/createFoodEvent/step/1");
-
   return (
     <div className="flex flex-col gap-5 p-5 ">
       <h2>Create a new event</h2>
 
       <div className="flex gap-3">
-        <RoundedLink href="/createFoodEvent/step/1" activeButtonState={activeButtonState}>1. Basics</RoundedLink>
-        <RoundedLink href="/createFoodEvent/step/2" activeButtonState={activeButtonState}>2. When & Where</RoundedLink>
-        <RoundedLink href="/createFoodEvent/step/3" activeButtonState={activeButtonState}>3. Pricing & Capacity</RoundedLink>
-        <RoundedLink href="/createFoodEvent/step/4" activeButtonState={activeButtonState}>4. Diet & Allergens</RoundedLink>
-        <RoundedLink href="/createFoodEvent/step/5" activeButtonState={activeButtonState}>5. Preview</RoundedLink>
+        <RoundedLink href="/createFoodEvent/step/1">1. Basics</RoundedLink>
+        <RoundedLink href="/createFoodEvent/step/2">2. When & Where</RoundedLink>
+        <RoundedLink href="/createFoodEvent/step/3">3. Pricing & Capacity</RoundedLink>
+        <RoundedLink href="/createFoodEvent/step/4">4. Diet & Allergens</RoundedLink>
+        <RoundedLink href="/createFoodEvent/step/5">5. Preview</RoundedLink>
       </div>
 
       <FormWizardProvider>{children}</FormWizardProvider>
--- src/foodclub-ui-next/components/core/roundedLink.tsx
+++ src/foodclub-ui-next/components/core/roundedLink.tsx
@@ -5,24 +5,20 @@ import { usePathname } from "next/navigation";
 export default function RoundedLink({
     children,
     href,
-    activeButtonState: [activeButton, setActiveButton],
     isActive,
 }: Readonly<{
     children: React.ReactNode;
     href: string,
-    activeButtonState: [string, (value: string) => void],
     isActive?: boolean
 }>) {
     const pathname = usePathname();
-    const computedIsActive =
-        isActive !== undefined ? isActive : (activeButton === href || pathname === href);
+    const computedIsActive = isActive !== undefined ? isActive : pathname === href;
 
     return (
         // bg-green-light - active, bg-gray-light - inActive
         <Link
             href={href}
             className={`p-2 ${computedIsActive ? "bg-green-light" : "bg-gray-light hover:bg-muted hover:text-foreground"} rounded-[50%] px-5 py-2 text-primary-green text-xs font-bold font-['Poppins'] transition-colors`}
-            onClick={e => setActiveButton(href)}
         >
             {children}
         </Link>
