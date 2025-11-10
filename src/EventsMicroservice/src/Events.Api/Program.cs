d58aad1c165b69714bcb6fc29be31ba9b2848bb6
daniil <danilkalinin@gmail.com>
2025-11-10 21:38:33 +0100

    fix versioning

--- src/EventsMicroservice/src/Events.Api/Program.cs
+++ src/EventsMicroservice/src/Events.Api/Program.cs
@@ -7,7 +7,7 @@ namespace Events.Api;
 
 public class Program
 {
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
